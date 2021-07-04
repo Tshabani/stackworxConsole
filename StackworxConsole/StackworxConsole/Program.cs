9996b0c2350b64811a67616022be27358ba36ffe
Matimba Moses <Hlongwanimm@gmail.com>
2021-07-04 10:04:41 +0200

    Initial Commit

---
+++ StackworxConsole/StackworxConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackworxConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
